fb22010414b2764f6a71ce0491f927ffa7492254
spustoff <vyacheslavpustov05@gmail.com>
2023-09-24 16:55:05 +0300

    Initial Commit

---
+++ App196/App196App.swift
@@ -0,0 +1,17 @@
+//
+//  App196App.swift
+//  App196
+//
+//  Created by Вячеслав on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct App196App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
